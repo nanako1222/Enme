21fec506a72f5bae5e9f6465d13abff3def04aae
EC2 Default User <ec2-user@ip-172-31-30-121.ap-northeast-1.compute.internal>
2022-10-14 13:38:07 +0000

    Create view of admin/allergy

--- app/controllers/admin/allergies_controller.rb
+++ app/controllers/admin/allergies_controller.rb
@@ -1,8 +1,16 @@
 class Admin::AllergiesController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
   end
 
   def create
+    @allergy = Allergy.new(allergy_params)
+    if @allergy.save
+      redirect_to admin_allergies_path(@allergy.id), notice: 'アレルゲンの追加に成功しました'
+    else
+      render :index
+    end
   end
 
   def edit
@@ -10,4 +18,9 @@ class Admin::AllergiesController < ApplicationController
 
   def update
   end
+
+    private
+    def allergy_params
+      params.require(:allergy).permit(:allergen)
+    end
 end
