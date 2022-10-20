3414f3e1a9074aefaccb8b105fe24accf94ea3e2
EC2 Default User <ec2-user@ip-172-31-30-121.ap-northeast-1.compute.internal>
2022-10-20 11:33:02 +0000

    Change view of application.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,6 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    # binding.pry
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :state_id, :area_id, :telephone_number])
   end
 end
