e596b09d505458d290aa3dade0d3b1a588c03c44
nanako1222 <nanakokoba75@gmail.com>
2022-10-23 10:58:51 +0000

    Change view of restaurant/menus

--- app/controllers/restaurant/menus_controller.rb
+++ app/controllers/restaurant/menus_controller.rb
@@ -1,22 +1,45 @@
 class Restaurant::MenusController < ApplicationController
   def index
+    @menu = Menu.new
+    @menus = Menu.page(params[:page])
   end
 
   def edit
+    @menu = Menu.find(params[:id])
   end
 
   def update
+    @menu = Menu.find(params[:id])
+    if @item.update(menu_params)
+      redirect_to restaurant_menus_path(@menu.id), notice: 'Product was successfully updated'
+    else
+      render :show
+    end
   end
 
   def new
+    @menu = Menu.new
   end
 
   def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect_to restaurant_menus_path(@menu.id), notice: 'Product was successfully created'
+    else
+      render :new
+    end
   end
 
   def show
+    @menu = Menu.find(params[:id])
+    @menu.image
   end
 
   def destroy
+  end
+  
+  private
+  def menu_params
+    params.require(:menu).permit(:image, :name, :introduction, :price, :allergen)
   end
 end
