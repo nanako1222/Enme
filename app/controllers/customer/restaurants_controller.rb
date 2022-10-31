a1a5a8653467b6fa77c00d1ac94e71d67085ddc7
nanako1222 <nanakokoba75@gmail.com>
2022-10-31 11:54:45 +0000

    Change restaurants_controller.rb of customer

--- app/controllers/customer/restaurants_controller.rb
+++ app/controllers/customer/restaurants_controller.rb
@@ -1,9 +1,9 @@
 
 
 class Customer::RestaurantsController < ApplicationController
-  
+
   before_action :set_search_query, only: :index
-  
+
   def show
   end
 
@@ -11,22 +11,23 @@ class Customer::RestaurantsController < ApplicationController
     #indexアクション内で検索画面のチェックボックスで検索を行いたい.
     #検索画面のアレルギーにチェックがついていないかつ顧客が持つアレルギーのチェックがないメニューを持つレストランを検索したい
     #1こでもアレルギーが入っているメニューが存在したらそれをはぶいて検索する
-  
+
     menus = if @allergy_ids.blank?
               Menu.all
             else
               Menu.all.reject do |menu|
-      
-                menu.allergies.pluck(:allergen).any? { |allergen| allergen.in?(@allergy_ids) }
+
+                menu.allergies.pluck(:id).any? { |allergen| allergen.in?(@allergy_ids) }
               end
             end
-    
+
     @restaurants = Restaurant.where(id: menus.pluck(:restaurant_id) ,state_id: @state_id, area_id: @area_id )
-    
   end
 
   def create
-    menu_ids = Menu.joins(:allergies).where(allergis: {id: params[:allergy_id]}).distinct.pluck(:id)
+    @allergy_id = params[:allergy_id]
+    #menu_ids = Menu.joins(:allergies).where(allergis: {id: params[:allergy_id]}).distinct.pluck(:id)
+    menu_ids = Menu.joins(:allergies).where(allergis: {id: @allergy_id}).distinct.pluck(:id)
     @restaurant.joins (:menus).where.not(menus:{id:menu_ids})
   end
 
@@ -39,13 +40,22 @@ class Customer::RestaurantsController < ApplicationController
     if request.xhr?
       render partial: 'areas', locals: {ms_pref_id: params[:ms_pref_id]}
     end
+    # if request.xhr?
+    #   render partial: 'areas', locals: {ms_pref_id: @ms_pref_id}
+    # end
   end
-  
+
   private
-  
+  # def set_create_query
+  #   @allergy_id = params[:allergy_id]
+  # end
+
+  # def set_customer_farm_area
+  #   @ms_pref_id = params[:ms_pref_id]
+  # end
 
   def set_search_query
-    @allergy_ids = params[:allergies]
+    @allergy_ids = params[:allergies].map(&:to_i)
     @state_id = params[:state_id]
     @area_id = params.dig(:customer, :area_id)
   end
