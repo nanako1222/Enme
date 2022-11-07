280cdd0736389cdc6cb71c7014f310b85f83ac51
nanako1222 <nanakokoba75@gmail.com>
2022-11-07 13:09:35 +0000

    Change menus_controller.rb of customer

--- app/controllers/customer/menus_controller.rb
+++ app/controllers/customer/menus_controller.rb
@@ -1,13 +1,22 @@
 class Customer::MenusController < ApplicationController
-    before_action :authenticate_restaurant!
+
   def index
-    @restaurants = Restaurant.all
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    # @restaurants = Restaurant.all
+    @menu = Menu.new
+    @menus = @restaurant.menus.page(params[:page])
+    @menu_count = @menus.count
   end
 
   def show
     @restaurant = current_customer
+    @menu = Menu.find(params[:id])
     @state = State.find(@customer.state_id).state
     @area = Area.find(@customer.area_id).area
   end
 
+  private
+  def menu_params
+    params.require(:menu).permit(:image, :name, :introduction, :price, allergy_ids: [])
+  end
 end
