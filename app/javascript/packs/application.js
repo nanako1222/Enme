4b173bc4b4dfcbc40e343be4c44d890b51d43627
辻 那菜子 <tsujinanako@MacBook-Air.local>
2026-03-04 00:39:58 +0900

    Merge area.js into application.js for guaranteed loading

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,35 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
-// jQueryをグローバル（window）に即座に登録
 import jQuery from "jquery"
-window.$ = window.jQuery = jQuery;
-
 import "popper.js"
 import "bootstrap"
-import "../stylesheets/application"
-import '@fortawesome/fontawesome-free/js/all'
 
-// 最後に自分のJSを読み込む
-import "./area.js"
+// グローバル設定
+window.$ = window.jQuery = jQuery;
 
 Rails.start()
 ActiveStorage.start()
+
+// --- ここに地域のプルダウン機能を直接書く ---
+$(document).on('turbolinks:load', function() {
+  console.log("Application.js is running!");
+
+  $(document).off('change', '[id*="state_id"]').on('change', '[id*="state_id"]', function() {
+    var stateId = $(this).val();
+    var elementId = $(this).attr('id');
+    console.log("State dropdown changed: " + elementId);
+
+    var type = elementId.includes("customer") ? "customer" : "restaurant";
+    var targetSelector = elementId.includes("customer") ? "#areas_select_customer" : "#areas_select";
+
+    $.ajax({
+      url: '/areas',
+      type: 'GET',
+      data: { type: type, state_id: stateId }
+    }).done(function(data){
+      console.log("Ajax success!");
+      $(targetSelector).html(data);
+    });
+  });
+});
