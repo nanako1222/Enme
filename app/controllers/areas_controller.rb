f96ee0960b6f6aef5fd0e92fa19ee8715c6fdf4d
辻 那菜子 <tsujinanako@MacBook-Air.local>
2026-03-04 00:57:00 +0900

    Fix: Area selection by rendering options directly in controller

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,7 +1,15 @@
 class AreasController < ApplicationController
   def index
+    # 送られてきた都道府県IDに紐づく地域を取得
     @areas = Area.where(state_id: params[:state_id])
-    @type = params[:type]
-    render layout: false
+    
+    # セレクトボックスの中身（HTML）を文字列として作成
+    options = "<option value=''>選択してください</option>"
+    @areas.each do |area|
+      options += "<option value='#{area.id}'>#{area.name}</option>"
+    end
+
+    # HTML形式で文字列のみを返す（ビューファイルを使わない設定）
+    render inline: options, layout: false
   end
 end
