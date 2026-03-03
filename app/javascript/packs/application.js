1813462de0f09386c687e3edc5d81aab3a5ba290
辻 那菜子 <tsujinanako@MacBook-Air.local>
2026-03-03 21:46:56 +0900

    Final fix: Restore logo and pull-down functionality

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,27 +1,16 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import Rails from "@rails/ujs"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 
-//= require jquery
-//= require jquery_ujs
+import "jquery"
+import "popper.js"
+import "bootstrap"
 
-import Rails from "@rails/ujs";
-//import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
-import "jquery";
-import "popper.js";
-import "bootstrap";
-import "../stylesheets/application";
-
-import '@fortawesome/fontawesome-free/js/all';
-import './area.js'
-
-const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
+import "../stylesheets/application"
+import '@fortawesome/fontawesome-free/js/all'
 
 Rails.start()
-//Turbolinks.start()
 ActiveStorage.start()
 
+// jQueryをグローバルに展開（これが無いとプルダウンが動きません）
+window.$ = window.jQuery = require('jquery');
