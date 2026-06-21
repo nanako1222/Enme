6a2dcb2fe5f8b3c467f34c7cfde16cba1d6e2656
辻 那菜子 <tsujinanako@MacBook-Air.local>
2026-06-22 01:11:11 +0900

    Clear image attachments to force clean re-attach on next build

---
+++ db/migrate/20260622000000_clear_image_attachments.rb
@@ -0,0 +1,8 @@
+class ClearImageAttachments < ActiveRecord::Migration[6.1]
+  def up
+    ActiveStorage::Attachment.where(record_type: %w[Restaurant Menu]).delete_all
+  end
+
+  def down
+  end
+end
