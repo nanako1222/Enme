6925af970ea34471bddc8112cd8e5f9f8252d46f
辻 那菜子 <tsujinanako@MacBook-Air.local>
2026-06-23 09:41:03 +0900

    Redesign top page: feature icons, step section, allergen grid, lighter footer

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -1,7 +1,12 @@
 class Customer::HomesController < ApplicationController
+  MANDATORY_ALLERGENS = %w[卵 えび かに そば 落花生 小麦 乳 くるみ].freeze
+
   def top
     @customer = Customer.new
-    @allergies = Allergy.all
+    @allergies = Allergy.all.sort_by { |a|
+      idx = MANDATORY_ALLERGENS.index(a.allergen)
+      idx.nil? ? MANDATORY_ALLERGENS.length : idx
+    }
     @restaurants = Restaurant.includes(:state, :area, menus: :allergies)
                              .where(is_valid: true)
                              .order(created_at: :desc)
